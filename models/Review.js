4498b8067454a740adfb6316dca3462296ebe0d6
Pierre R Galette <pierre.galette@protonmail.com>
2024-01-18 19:56:52 -0500

    Updated Review schema

--- models/Review.js
+++ models/Review.js
@@ -2,10 +2,15 @@ const { model, Schema, default: mongoose } = require("mongoose");
 
 const reviewSchema = new Schema(
   {
+    title: { type: String },
     review: { type: String },
     cocktail: {
       type: mongoose.Types.ObjectId,
       ref: "Cocktail",
+    },
+    userOwner: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
     },
   },
   {
